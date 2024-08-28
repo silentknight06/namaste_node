4cd92c733934069641fbca5210eac1102670ed45
Deepak Kumar <deepakroy7393@gmail.com>
2024-08-28 07:39:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+ const {x, sum1}  = require("./sum")
+
+var a = 10;
+var b = 20;
+  sum1(a, b);
+
---
+++ sum.js
@@ -0,0 +1,8 @@
+function sum1(a, b){
+    const  add = a+b;
+    console.log(add);
+}
+
+var x= "deepak";
+
+module.exports = {x, sum1};
